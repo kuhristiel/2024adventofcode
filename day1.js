1bd3df35e0e379dff65bae7d78063a0471b58185
Christie Lin <christiejlin@gmail.com>
2024-12-11 16:10:21 -0800

    Updated day1 file to include part 1 and 2, cleared day2 file

--- day1.js
+++ day1.js
@@ -31,6 +31,13 @@ function distLists( {left, right} ) {
    return dist;
 }
 
+function simScore({left, right}) {
+
+    return left.reduce((simScore, x) => 
+        (right.reduce((matchesToX, y) => matchesToX + (x===y), 0) * x) + simScore, 0)
+}
+
 // Example usage
-const { left, right } = parseLists('day1.txt');
+const { left, right } = parseLists('C:/Users/yiwan/Documents/Christie/Code/2024adventofcode/input/day1.txt');
 console.log(distLists({left, right}));
+console.log(simScore({left, right}));
